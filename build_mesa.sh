06f63149bba562ac48910109c3e68dbef550b012
Yusen <1405481963@qq.com>
2024-08-11 02:51:45 +0000

    build deb package for mesa

--- build_mesa.sh
+++ build_mesa.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-# 使用readlink -f将~转换为绝对路径
+# 使用 readlink -f 将 ~ 转换为绝对路径
 home_path=$(readlink -f ~)
 cd ./mesa
-sudo cp -r /usr/include/drm/* /usr/include \
+sudo cp -r /usr/include/drm/* /usr/include
 
 commit_short=$(git rev-parse --short HEAD)
 commit=$(git rev-parse HEAD)
@@ -14,29 +14,56 @@ minor=$(echo $version | cut -d "," -f 3 | xargs)
 patch=$(awk -F'VK_HEADER_VERSION |\n#define' '{print $2}' <<< $(cat include/vulkan/vulkan_core.h) | xargs)
 vulkan_version="$major.$minor.$patch"
 
+# 获取关键依赖项的版本
+libc_version=$(dpkg-query -W -f='${Version}' libc6 | awk -F'-' '{print $1}' | tr -d '\n')
+libllvm_package=$(dpkg -l | grep '^ii' | grep -E 'libllvm[0-9]' | awk '{print $2}' | sort -V | tail -n 1 | cut -d ':' -f 1)
+
+# 应用补丁
 patch -p1 < ../turnip-patches/fix-for-anon-file.patch
 patch -p1 < ../turnip-patches/fix-for-getprogname.patch
 patch -p1 < ../turnip-patches/zink_fixes.patch
 patch -p1 < ../turnip-patches/dri3.patch
 
-#编译64位turnip+zink+解码库+镓九
+# 清理 /usr/local
+sudo rm -rf /usr/local/*
 
-CC=clang CXX=clang++ meson b -Dgallium-drivers=virgl,zink,llvmpipe,d3d12 -Dvulkan-drivers=freedreno,swrast -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dllvm=enabled -Dxlib-lease=enabled -Dgles2=enabled -Dgallium-nine=true -Dgallium-opencl=icd -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled  -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dgallium-vdpau=enabled -Dopengl=true -Dosmesa=true -Dpower8=enabled -Degl-native-platform=drm -Db_lto=true -Dcpp_args="-Wno-typedef-redefinition -flto -O3" -Dc_args="-Wno-typedef-redefinition -flto -O3" 
+# 编译 64 位 turnip + zink + 解码库 + 镓九
+CC=clang CXX=clang++ meson b -Dgallium-drivers=virgl,zink,llvmpipe,d3d12 -Dvulkan-drivers=freedreno,swrast -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dllvm=enabled -Dxlib-lease=enabled -Dgles2=enabled -Dgallium-nine=true -Dgallium-opencl=icd -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled  -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dgallium-vdpau=enabled -Dopengl=true -Dosmesa=true -Dpower8=enabled -Degl-native-platform=drm -Db_lto=true -Dcpp_args="-Wno-typedef-redefinition -flto -O3" -Dc_args="-Wno-typedef-redefinition -flto -O3"
 
 cd b
-ninja 
+ninja
+
+# 安装 64 位构建
+sudo ninja install
+
+# 编译 32 位 turnip + zink
 cd ..
-#编译32位turnip + zink
-#cd /tmp/mesa
 meson build32 --cross-file=cross32.txt --libdir=lib/arm-linux-gnueabihf -Dgallium-drivers=virgl,zink,d3d12 -Dvulkan-drivers=freedreno -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dxlib-lease=enabled -Dgles2=enabled -Degl-native-platform=drm -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dopengl=true -Dpower8=enabled -Db_lto=true -Dcpp_args="-O3 -flto" -Dc_args="-O3 -flto"
 
 cd build32
 ninja
-sudo rm -rf /usr/local
-sudo mkdir /usr/local
+
+# 安装 32 位构建
 sudo ninja install
-cd ..
-cd b
-sudo ninja install
+
+# 打包为 .tgz 文件
+cd ${home_path}
 mkdir -p ${home_path}/upload/
-tar -czf ${home_path}/upload/${mesa_version}-${commit_short}.tgz /usr/local
+tar -czf ${home_path}/upload/mesa-${mesa_version}-${commit_short}.tgz -C /usr/local .
+
+# 创建 DEBIAN 控制文件
+mkdir -p ${home_path}/mesa_deb/DEBIAN
+cat <<EOF > ${home_path}/mesa_deb/DEBIAN/control
+Package: mesa-adreno
+Version: ${mesa_version}-${commit_short}
+Architecture: arm64
+Maintainer: Yusen <1405481963@qq.com>
+Depends: libc6 (>= ${libc_version}), ${libllvm_package}
+Description: Mesa 3D graphics library with Turnip and Zink for Adreno GPUs
+EOF
+
+# 将已安装的文件复制到 deb 目录结构中
+sudo cp -a /usr/local/* ${home_path}/mesa_deb/
+
+# 打包为 .deb 文件
+dpkg-deb --build ${home_path}/mesa_deb ${home_path}/upload/mesa-${mesa_version}-${commit_short}.deb
