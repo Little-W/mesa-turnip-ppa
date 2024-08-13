6bae218e474a41513c6c1ab7aced65d14c7ae1f4
Yusen <1405481963@qq.com>
2024-08-13 05:19:27 +0000

    update mesa building script

--- build_mesa.sh
+++ build_mesa.sh
@@ -4,6 +4,29 @@
 proj_path=$(pwd)
 home_path=$(readlink -f ~)
 
+create_deb_package () {
+    local dest_dir=$1
+    local package_name=$2
+    local install_prefix=$3
+
+    mkdir -p ${dest_dir}/DEBIAN
+    cat <<EOF > ${dest_dir}/DEBIAN/control
+Package: ${package_name}
+Version: ${mesa_version}-${commit_short}
+Architecture: arm64
+Maintainer: Yusen <1405481963@qq.com>
+Depends: libc6 (>= ${libc_version}), ${libllvm_package}
+Description: Mesa 3D graphics library with Turnip and Zink for Adreno GPUs
+EOF
+
+    # 将已安装的文件复制到 deb 目录结构中
+    sudo mkdir -p ${dest_dir}${install_prefix}
+    sudo cp -a /usr/local/* ${dest_dir}${install_prefix}
+
+    # 打包为 .deb 文件
+    dpkg-deb --build ${dest_dir} ${home_path}/upload/${package_name}-${mesa_version}-${commit_short}.deb
+}
+
 create_pkg_package () {
     local dest_dir=$1
     local package_name=$2
@@ -62,11 +85,20 @@ done
 sudo rm -rf /usr/local/*
 
 # 编译 64 位 turnip + zink + 解码库 + 镓九
-CC=clang CXX=clang++ meson b --libdir=lib -Dgallium-drivers=virgl,zink,llvmpipe,freedreno,d3d12 -Dvulkan-drivers=freedreno,swrast -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dllvm=enabled -Dxlib-lease=enabled -Dgles1=enabled -Dgles2=enabled -Dgallium-nine=true -Dgallium-opencl=icd -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled -Dgbm=enabled -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dgallium-vdpau=enabled -Dopengl=true -Dosmesa=true -Dpower8=enabled -Degl-native-platform=x11 -Db_lto=true -Dcpp_args="-Wno-typedef-redefinition -flto -O3" -Dc_args="-Wno-typedef-redefinition -flto -O3"
+CC=clang CXX=clang++ meson b --libdir=lib -Dgallium-drivers=virgl,zink,llvmpipe,freedreno,d3d12 -Dvulkan-drivers=freedreno,swrast -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dllvm=enabled -Dxlib-lease=enabled -Dgles1=disabled -Dgles2=enabled -Dgallium-nine=true -Dgallium-opencl=icd -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled -Dgbm=enabled -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dgallium-vdpau=enabled -Dopengl=true -Dosmesa=true -Dpower8=enabled -Degl-native-platform=x11 -Dglvnd=disabled -Db_lto=true -Dcpp_args="-Wno-typedef-redefinition -flto -O3" -Dc_args="-Wno-typedef-redefinition -flto -O3"
 
 cd b
 ninja
 sudo ninja install
+
+# 打包为 .tgz 文件，保留 /usr/local 目录结构
+cd ${home_path}
+mkdir -p ${home_path}/upload/
+tar -czf ${home_path}/upload/mesa-${mesa_version}-${commit_short}-lto.tgz -C / usr/local
+
+# 打包 LTO 版本的 .deb 包
+create_deb_package "${home_path}/mesa_deb_lto_root" "mesa-adreno-lto-root" "/"
+create_deb_package "${home_path}/mesa_deb_lto_local" "mesa-adreno-lto-local" "/usr/local"
 
 # 打包 LTO 版本的 Arch Linux .pkg.tar.gz 包
 create_pkg_package "${home_path}/mesa_pkg_lto_root" "mesa-adreno-lto-root" "/"
