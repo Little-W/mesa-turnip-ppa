44a4e6e94ee82d4ea38ffc75763fcde333b3e57a
Yusen <1405481963@qq.com>
2024-08-13 03:46:16 +0000

    build for archlinux only

--- box86.sh
+++ box86.sh
@@ -7,18 +7,7 @@ sudo rm -rf /usr/local/*
 mkdir -p ~/env_workspace
 cd ~/env_workspace
 
-# 删除已有的 box86 和 box64
-rm -rf box86
-rm -rf box64
-
-# 克隆最新的 box86 和 box64 源代码
-git clone --depth 1 --single-branch https://github.com/ptitSeb/box86.git
-cd box86
-git fetch --tags
-box86_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
-box86_commit=$(git rev-parse --short HEAD)
-cd ..
-
+# 克隆最新的 box64 源代码
 git clone --depth 1 --single-branch https://github.com/ptitSeb/box64.git
 cd box64
 git fetch --tags
@@ -66,47 +55,6 @@ EOF
     tar -czf ${home_path}/upload/${package_name}-${version}-${arch}.pkg.tar.gz -C pkg .
 }
 
-# 构建和安装 box86 (armv7h)
-cd box86
-mkdir build
-cd build
-cmake .. -DARM64=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBAD_SIGNAL=ON -DARM_DYNAREC=ON -Dmfpu=neon-fp-armv8 -Dmfloat-abi=hard -DSD845=ON
-make -j8
-
-# 安装到临时目录
-DESTDIR=${home_path}/box86_install
-make install DESTDIR=${DESTDIR}
-
-# 安装到默认目录（/usr/local）
-sudo make install
-
-# 替换临时目录中的二进制文件
-sudo cp /usr/local/bin/box86 ${DESTDIR}/usr/local/bin/
-
-# 打包成 .tgz 文件
-cd ${home_path}
-tar -czf ${home_path}/upload/box86-${box86_tag}-${box86_commit}.tgz -C ${DESTDIR} .
-
-# 打包成 .deb 文件
-deb_version=$(echo ${box86_tag} | sed 's/^v//')
-cd ${home_path}
-mkdir -p ${DESTDIR}/DEBIAN
-cat <<EOF > ${DESTDIR}/DEBIAN/control
-Package: box86
-Version: ${deb_version}-${box86_commit}
-Architecture: armhf
-Maintainer: Yusen <1405481963@qq.com>
-Depends: libc6 (>= ${libc_version})
-Description: Box86 - A dynamic recompiler for running x86 applications on ARM platforms.
-EOF
-dpkg-deb --build ${DESTDIR} ${home_path}/upload/box86-${box86_tag}-${box86_commit}.deb
-
-# 打包成 .pkg.tar.gz 文件 (armv7h for Arch Linux)
-create_pkg_package "${home_path}/box86_pkg" "box86" "/" "${deb_version}-${box86_commit}" "armv7h" "glibc"
-
-# 清理临时目录
-rm -rf ${DESTDIR}
-
 # 构建和安装 box64 (aarch64)
 cd ~/env_workspace/box64
 mkdir build
@@ -128,22 +76,5 @@ sudo cp /usr/local/bin/box64 ${DESTDIR}/usr/local/bin/
 cd ${home_path}
 tar -czf ${home_path}/upload/box64-${box64_tag}-${box64_commit}.tgz -C ${DESTDIR} .
 
-# 打包成 .deb 文件
-deb_version=$(echo ${box64_tag} | sed 's/^v//')
-cd ${home_path}
-mkdir -p ${DESTDIR}/DEBIAN
-cat <<EOF > ${DESTDIR}/DEBIAN/control
-Package: box64
-Version: ${deb_version}-${box64_commit}
-Architecture: arm64
-Maintainer: Yusen <1405481963@qq.com>
-Depends: libc6 (>= ${libc_version})
-Description: Box64 - A dynamic recompiler for running x86_64 applications on ARM64 platforms.
-EOF
-dpkg-deb --build ${DESTDIR} ${home_path}/upload/box64-${box64_tag}-${box64_commit}.deb
-
 # 打包成 .pkg.tar.gz 文件 (aarch64 for Arch Linux)
 create_pkg_package "${home_path}/box64_pkg" "box64" "/" "${deb_version}-${box64_commit}" "aarch64" "glibc"
-
-# 清理临时目录
-rm -rf ${DESTDIR}
--- build_mesa.sh
+++ build_mesa.sh
@@ -4,34 +4,12 @@
 proj_path=$(pwd)
 home_path=$(readlink -f ~)
 
-create_deb_package () {
-    local dest_dir=$1
-    local package_name=$2
-    local install_prefix=$3
-
-    mkdir -p ${dest_dir}/DEBIAN
-    cat <<EOF > ${dest_dir}/DEBIAN/control
-Package: ${package_name}
-Version: ${mesa_version}-${commit_short}
-Architecture: arm64
-Maintainer: Yusen <1405481963@qq.com>
-Depends: libc6 (>= ${libc_version}), ${libllvm_package}
-Description: Mesa 3D graphics library with Turnip and Zink for Adreno GPUs
-EOF
-
-    # 将已安装的文件复制到 deb 目录结构中
-    sudo mkdir -p ${dest_dir}${install_prefix}
-    sudo cp -a /usr/local/* ${dest_dir}${install_prefix}
-
-    # 打包为 .deb 文件
-    dpkg-deb --build ${dest_dir} ${home_path}/upload/${package_name}-${mesa_version}-${commit_short}.deb
-}
-
 create_pkg_package () {
     local dest_dir=$1
     local package_name=$2
     local install_prefix=$3
-
+    
+    mkdir -p ${home_path}/upload
     mkdir -p ${dest_dir}/pkg/metadata
 
     # 创建 `desc` 文件
@@ -84,7 +62,7 @@ done
 sudo rm -rf /usr/local/*
 
 # 编译 64 位 turnip + zink + 解码库 + 镓九
-CC=clang CXX=clang++ meson b -Dgallium-drivers=virgl,zink,llvmpipe,freedreno,d3d12 -Dvulkan-drivers=freedreno,swrast -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dllvm=enabled -Dxlib-lease=enabled -Dgles1=disabled -Dgles2=enabled -Dgallium-nine=true -Dgallium-opencl=icd -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled -Dgbm=enabled -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dgallium-vdpau=enabled -Dopengl=true -Dosmesa=true -Dpower8=enabled -Degl-native-platform=x11 -Db_lto=true -Dxmlconfig=disabled -Dcpp_args="-Wno-typedef-redefinition -flto -O3" -Dc_args="-Wno-typedef-redefinition -flto -O3"
+CC=clang CXX=clang++ meson b --libdir=lib -Dgallium-drivers=virgl,zink,llvmpipe,freedreno,d3d12 -Dvulkan-drivers=freedreno,swrast -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dllvm=enabled -Dxlib-lease=enabled -Dgles1=disabled -Dgles2=enabled -Dgallium-nine=true -Dgallium-opencl=icd -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled -Dgbm=enabled -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dgallium-vdpau=enabled -Dopengl=true -Dosmesa=true -Dpower8=enabled -Degl-native-platform=x11 -Db_lto=true -Dxmlconfig=disabled -Dcpp_args="-Wno-typedef-redefinition -flto -O3" -Dc_args="-Wno-typedef-redefinition -flto -O3"
 
 cd b
 ninja
@@ -93,19 +71,3 @@ sudo ninja install
 # 打包 LTO 版本的 Arch Linux .pkg.tar.gz 包
 create_pkg_package "${home_path}/mesa_pkg_lto_root" "mesa-adreno-lto-root" "/"
 create_pkg_package "${home_path}/mesa_pkg_lto_local" "mesa-adreno-lto-local" "/usr/local"
-
-cd ${proj_path}/mesa
-meson build32 --cross-file=cross32.txt --libdir=lib/arm-linux-gnueabihf -Dgallium-drivers=virgl,zink,freedreno,d3d12 -Dvulkan-drivers=freedreno -Dglx=dri -Dplatforms=x11,wayland -Dbuildtype=release -Dxlib-lease=enabled -Dgles1=disabled -Dgles2=enabled -Degl-native-platform=x11 -Degl=enabled -Dfreedreno-kmds=kgsl,msm -Ddri3=enabled -Dgbm=enabled -Dvulkan-beta=true -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc -Dglx-direct=true -Dtools=drm-shim,freedreno -Dopengl=true -Dpower8=enabled -Db_lto=true -Dxmlconfig=disabled -Dcpp_args="-O3 -flto" -Dc_args="-O3 -flto"
-cd build32
-ninja
-
-# 安装 32 位构建
-sudo ninja install
-# 打包为 .tgz 文件，保留 /usr/local 目录结构
-cd ${home_path}
-mkdir -p ${home_path}/upload/
-tar -czf ${home_path}/upload/mesa-${mesa_version}-${commit_short}-lto.tgz -C / usr/local
-
-# 打包 LTO 版本的 .deb 包
-create_deb_package "${home_path}/mesa_deb_lto_root" "mesa-adreno-lto-root" "/"
-create_deb_package "${home_path}/mesa_deb_lto_local" "mesa-adreno-lto-local" "/usr/local"
