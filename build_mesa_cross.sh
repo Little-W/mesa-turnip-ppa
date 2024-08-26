6b5a7416d138bdaa2a655f450918924719c59b73
Yusen <1405481963@qq.com>
2024-08-26 05:53:49 +0000

    update mesa building script

--- build_mesa_cross.sh
+++ build_mesa_cross.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -e
 # 记录项目路径
 proj_path=$(pwd)
 home_path=$(readlink -f ~)
@@ -10,10 +10,18 @@ else
     UPLOAD_DIR=/tmp/upload
 fi
 
+if [ "${ARCHLINUX}" = "1" ]; then
+    LIBDIR=" --libdir=lib"
+    SUFFIX="-arch"
+else
+    LIBDIR=''
+    SUFFIX=''
+fi
+
 BUILD_DIR=${UPLOAD_DIR}/local
 sudo mkdir -p ${UPLOAD_DIR}
 sudo chmod -R 777 ${UPLOAD_DIR}
-
+rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
 
 ## Flags changes in order to use ccache..
@@ -40,7 +48,7 @@ sudo mkdir -p ${BOOTSTRAP_PATH}/${proj_path}
 
 _bwrap () {
     bwrap --ro-bind "${BOOTSTRAP_PATH}" / --dev /dev --ro-bind /sys /sys \
-          --proc /proc --tmpfs /home --tmpfs /run --tmpfs /var \
+          --proc /proc --tmpfs /home --tmpfs /run \
           --tmpfs /mnt --tmpfs /media --bind "${BUILD_DIR}" /usr/local \
           --bind "${proj_path}" "${proj_path}" \
           --ro-bind /proc/sys/fs/binfmt_misc /proc/sys/fs/binfmt_misc \
@@ -52,6 +60,7 @@ _bwrap () {
           --setenv LD_LIBRARY_PATH "/tmp/arm64-deps/usr/lib/aarch64-linux-gnu:/lib/aarch64-linux-gnu:/usr/lib/aarch64-linux-gnu" \
           --setenv LC_ALL en_US.UTF-8 \
           --setenv LANGUAGE en_US.UTF-8 \
+          --setenv TMPDIR "${proj_path}" \
           --chdir "$(pwd)" \
           "$@"
 }
@@ -61,7 +70,6 @@ if [ ! -d "${BOOTSTRAP_PATH}" ] ; then
 	echo "Ubuntu Bootstrap is required for compilation!"
 	exit 1
 fi
-
 
 create_deb_package () {
     local dest_dir=$1
@@ -86,20 +94,7 @@ EOF
     dpkg-deb --build ${dest_dir} ${UPLOAD_DIR}/${package_name}-${mesa_version}-${commit_short}.deb
 }
 
-cd ${proj_path}
-# 克隆Mesa仓库
-git clone --depth=1 https://gitlab.freedesktop.org/mesa/mesa.git -b 24.2
-
-echo "---------------------------------------------------------"
-echo "     Start: Copy cross compile configuration"
-echo "---------------------------------------------------------"
-cp -f ./files/cross64.txt ./mesa/cross64.txt
-echo "---------------------------------------------------------"
-echo "      End: Copy cross compile configuration"
-echo "---------------------------------------------------------"
-
 cd ${proj_path}/mesa
-sudo cp -r /usr/include/drm/* /usr/include
 
 commit_short=$(git rev-parse --short HEAD)
 commit=$(git rev-parse HEAD)
@@ -110,21 +105,44 @@ minor=$(echo $version | cut -d "," -f 3 | xargs)
 patch=$(awk -F'VK_HEADER_VERSION |\n#define' '{print $2}' <<< $(cat include/vulkan/vulkan_core.h) | xargs)
 vulkan_version="$major.$minor.$patch"
 
-# 获取关键依赖项的版本
-libc_version=$(dpkg-query -W -f='${Version}' libc6 | awk -F'-' '{print $1}' | tr -d '\n')
-libllvm_package=$(dpkg -l | grep '^ii' | grep -E 'libllvm[0-9]' | awk '{print $2}' | sort -V | tail -n 1 | cut -d ':' -f 1)
-llvm_libs_version=$(dpkg-query -W -f='${Version}' ${libllvm_package} | awk -F'-' '{print $1}' | tr -d '\n')
+raw_libc_version=$(_bwrap dpkg-query -W -f='${Version}' libc6)
+libc_version=$(echo "$raw_libc_version" | awk -F'-' '{print $1}' | tr -d '\n')
+raw_libllvm_packages=$(_bwrap dpkg -l | grep '^ii' | grep -E 'libllvm[0-9]+')
+libllvm_package=$(echo "$raw_libllvm_packages" | awk '{print $2}' | sort -V | tail -n 1 | cut -d ':' -f 1)
 
-# 应用turnip-patches目录下的所有补丁
-for patch in ../turnip-patches/*.patch; do
-    patch -p1 < "$patch"
-done
+export CPPFLAGS="-D_FORTIFY_SOURCE=0"
 
-# 清理 /usr/local
-sudo rm -rf /usr/local/*
+export CFLAGS_BASE="-O3 -march=armv8.2-a+crc+simd+crypto -mtune=cortex-a77 -fno-math-errno -fno-trapping-math -funroll-loops -fno-semantic-interposition -fcf-protection=none -mharden-sls=none -fomit-frame-pointer"
 
+export CFLAGS_LLVM="-Wno-typedef-redefinition -mllvm -extra-vectorizer-passes -mllvm -enable-cond-stores-vec -mllvm -enable-loop-distribute -mllvm -enable-unroll-and-jam -mllvm -enable-loop-flatten -mllvm -interleave-small-loop-scalar-reduction -mllvm -unroll-runtime-multi-exit -mllvm -aggressive-ext-opt -mllvm -polly -mllvm -polly-ast-use-context -mllvm -polly-invariant-load-hoisting -mllvm -polly-run-inliner -mllvm -polly-vectorizer=stripmine -mllvm -polly-loopfusion-greedy=1 -mllvm -polly-reschedule=1 -mllvm -polly-postopts=1"
+
+export LDFLAGS_BASE="-O3"
+
+export LDFLAGS_LLVM="-fuse-ld=lld"
+
+if [ "${CLANG}" = "1" ]; then
+    export CFLAGS="${CFLAGS_BASE} ${CFLAGS_LLVM}"
+    export LDFLAGS="${LDFLAGS_BASE} ${LDFLAGS_LLVM}"
+    CROSSFILE_SUFFIX="_clang"
+    BUILDFILE_SUFFIX="-clang"
+else
+    export CFLAGS="${CFLAGS_BASE}"
+    export LDFLAGS="${LDFLAGS_BASE}"
+fi
+
+export CXXFLAGS="${CFLAGS}"
+export CCLDFLAGS="${LDFLAGS}"
+export CXXLDFLAGS="${LDFLAGS}"
+
+if [[ "$(printf '%s\n' "$mesa_version" "24.2" | sort -V | head -n1)" == "24.2" ]]; then
+    SOFTRENDERER="llvmpipe"
+else
+    SOFTRENDERER="swrast"
+fi
+
+rm -rf b
 # 编译 64 位 turnip + zink + 解码库 + 镓九
-_bwrap meson b --cross-file=cross64.txt -Dbuildtype=release -Dplatforms=x11,wayland -Ddri3=enabled -Dgallium-drivers=virgl,zink,llvmpipe,freedreno -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dimagination-srv=true -Dvulkan-layers=device-select -Dgles2=enabled -Dopengl=true -Dgbm=enabled -Dglx=dri -Dosmesa=true -Dpower8=enabled -Dxlib-lease=enabled -Dvulkan-beta=true -Dvideo-codecs=all -Degl-native-platform=x11 -Dglx-direct=true -Degl=enabled -Dglvnd=enabled -Db_lto=true -Dcpp_args="-Wno-narrowing -O3 -march=armv8.2-a+crc+simd+crypto -mtune=cortex-a76.cortex-a55" -Dc_args="-Wno-narrowing -Wno-incompatible-pointer-types -O3 -march=armv8.2-a+crc+simd+crypto -mtune=cortex-a76.cortex-a55"
+_bwrap meson b --cross-file=cross64${CROSSFILE_SUFFIX}.txt ${LIBDIR} -Dbuildtype=release -Dplatforms=x11,wayland -Ddri3=enabled -Dgallium-drivers=virgl,zink,${SOFTRENDERER},freedreno -Dvulkan-drivers=freedreno -Dfreedreno-kmds=msm,kgsl -Dimagination-srv=true -Dvulkan-layers=device-select -Dgles2=enabled -Dopengl=true -Dgbm=enabled -Dglx=dri -Dosmesa=true -Dpower8=enabled -Dxlib-lease=enabled -Dvulkan-beta=true -Dvideo-codecs=all -Degl-native-platform=x11 -Dglx-direct=true -Degl=enabled -Dglvnd=enabled -Db_lto=true -Dcpp_args="${CXXFLAGS} -Wno-narrowing" -Dc_args="${CFLAGS} -Wno-narrowing -Wno-incompatible-pointer-types"
 
 cd b
 _bwrap ninja
@@ -132,8 +150,8 @@ _bwrap ninja install
 
 # 打包为 .tgz 文件，保留 /usr/local 目录结构
 cd ${home_path}
-tar -czf ${UPLOAD_DIR}/mesa-${mesa_version}-${commit_short}-lto.tgz -C / ${BUILD_DIR}
+tar -czf ${UPLOAD_DIR}/mesa-${mesa_version}-${commit_short}${SUFFIX}${BUILDFILE_SUFFIX}.tgz -C / ${BUILD_DIR}
 
 # 打包 LTO 版本的 .deb 包
-create_deb_package "${home_path}/mesa_deb_lto_root" "mesa-adreno-lto-root" "/"
-create_deb_package "${home_path}/mesa_deb_lto_local" "mesa-adreno-lto-local" "/usr/local"
+create_deb_package "${home_path}/mesa_deb_root" "mesa-adreno-root${SUFFIX}${BUILDFILE_SUFFIX}" "/"
+create_deb_package "${home_path}/mesa_deb_local" "mesa-adreno-local${SUFFIX}${BUILDFILE_SUFFIX}" "/usr/local"
